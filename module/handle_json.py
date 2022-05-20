07b45ebc49065a7560531815261a4889debe6586
periergeia <88389522+periergeia@users.noreply.github.com>
2022-05-20 18:52:05 +0200

    ajout de docstring
    test pylint réussi, youpi !

--- module/handle_json.py
+++ module/handle_json.py
@@ -1,12 +1,19 @@
+"""module permettant de gérer un fichier un fichier JSON."""
+# ## déplacement possible vers useful.py
+
+
+# importations de modules
 import json
 try:
     from useful import file_directory_issue
-except:
+except ModuleNotFoundError:
     from module.useful import file_directory_issue
 
 
 @file_directory_issue()
 def get_dict(file_path):
+    """renvoie un dictionnaire, celui converti depuis le fichier
+    JSON spécifié  par `file_path`."""
     with open(file_path, encoding="utf-8") as file:
         data = json.load(file)
     file.close()
@@ -14,7 +21,8 @@ def get_dict(file_path):
 
 @file_directory_issue()
 def write(file_path, dict_key, new_data):
-    """"""
+    """modfifie le fichier JSON `file_path` en remplaçant la valeur
+    de la clef `dict_key` par `new_data`."""
     data = get_dict(file_path=file_path)
     data[dict_key] = new_data
     with open(file_path, "w", encoding="utf-8") as file:
@@ -22,6 +30,8 @@ def write(file_path, dict_key, new_data):
 
 
 def get_value(file_path, key):
-    """"""
+    """permet d'obtenir une valeur (sous forme de dictionnaire
+    au vu de la structure des fichiers JSON) de `file_path`
+    d'après la clef `key`"""
     data = get_dict(file_path=file_path)
     return data[key]
